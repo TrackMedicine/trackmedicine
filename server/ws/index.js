f0ce996070e7a66ed42474fbbacb3c02a8eea895
Desiree Santos <desisant@gmail.com>
2016-09-11 20:06:39 -0300

    Indentation tab size 2

--- server/ws/index.js
+++ server/ws/index.js
@@ -8,7 +8,7 @@ exports.mqtt_connection = function (credential,http, port){
 	//Begin TODO:Spike works, next step: Create test, extract logic
 	mqttclient.on('connect', function () {
  		mqttclient.subscribe(topic)
-  });
+  	});
 
 	io = require('socket.io').listen(http, require('config').server);
 	io.sockets.on('connection', function (socket) {
