cf5006093d82a553dffcd9d8eff812a68fc79c6a
Desiree Santos <desisant@gmail.com>
2016-09-25 00:26:20 -0300

    Refactoring connection file

--- server/connection/index.js
+++ server/connection/index.js
@@ -1,26 +1,45 @@
-var io
-var mqttclient
+let socket = require('socket.io')
+let mqtt = require('mqtt')
+let configserver = require('config').server
+const TEMPERATURE = 'temperature'
+const EMIT_NAME = 'mqtt'
 
-exports.mqtt = function (credential,http){
-	mqttclient = require('mqtt').connect(credential)
-	topic = 'temperature'
 
-	//Begin TODO:Spike works, next step: Create test, extract logic
-	mqttclient.on('connect', function () {
- 		mqttclient.subscribe(topic)
-  	});
+exports.mqtt = credential => {
+ 	mqttclient = mqtt.connect(credential)
 
-	io = require('socket.io').listen(http, require('config').server);
-	io.sockets.on('connection', function (socket) {
-		io.sockets.emit('mqtt', '');
-	});
-
-	mqttclient.on('message', function(topic,message) {
-    	io.sockets.emit('mqtt', message.toString());
-	});
-	//End
+ 	mqttclient.on('connect', () => {
+ 		console.log('Mqtt connected')
+ 	})
+ 	return mqttclient
 }
 
-exports.mongodb = function(uri) {
-	require('mongoose').connect(uri);
+exports.subscribe = (mqttclient, topic = TEMPERATURE) => {
+ 	mqttclient.on('connect', () => {
+ 		mqttclient.subscribe(topic)
+  })
+}
+
+exports.read_msg = (mqttclient, io, topic, msg) => {
+	mqttclient.on('message', topic, msg => {
+    	io.sockets.emit(topic, msg.toString())
+	})
+}
+
+exports.socket = (http) => {
+	io = socket.listen(http, configserver)
+	io.sockets.on('connection', socket => {
+		console.log('socket connected')
+	})
+	return io
+}
+
+exports.emit_start = (io, name = EMIT_NAME) => {
+	io.sockets.on('connection', socket => {
+		socket.emit(name, '')
+	})
+}
+
+exports.mongodb = uri => {
+	require('mongoose').connect(uri)
 }
