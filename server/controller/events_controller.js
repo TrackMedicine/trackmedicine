c4527834b2189964c8442c13682a53a5ee540038
Desiree Santos <desisant@gmail.com>
2016-09-27 02:41:28 -0300

    Addind event sourcing first step

---
+++ server/controller/events_controller.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let Event = require('../model/event')
+
+class Events {
+	constructor() {
+		this.events = new Array()
+	}
+
+	getEvents() {return this.events}
+
+	push(event) {
+		let newEvent = new Event(event.name, event.data)
+		this.events.push(newEvent)
+		return this.events
+	}
+}
+
+module.exports = Events;
---
+++ test/unit/controller/events_controller.test.js
@@ -0,0 +1,18 @@
+'use strict';
+
+let assert = require('assert')
+let Event = require('../../../server/model/event')
+let EventsController = require('../../../server/controller/events_controller')
+
+describe('Events', () => {
+
+    it('has only one event', () => {
+     	
+     	const event = new Event('event type', {})
+     	let events = new EventsController()
+
+    	let result = events.push(event)
+
+      assert.equal(result.length, 1);
+    })
+})
