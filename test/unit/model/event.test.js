6da69a7d2dcfe01eac3167b6a65957f9e59217f4
Desiree Santos <desisant@gmail.com>
2016-10-01 11:49:24 -0300

    Fixing unit test

--- test/unit/model/event.test.js
+++ test/unit/model/event.test.js
@@ -8,10 +8,12 @@ describe('Event', function() {
   it('holds the given data', function() {
   	const name = 'event type'
   	const data = {some: 'data', here: 'too'}
+  	const timestamp = Date.now
 
-  	var event = new Event(name, data)
+  	var event = new Event(name, data, timestamp)
 
-  	assert.deepEqual(event.name, 'event type')
-  	assert.deepEqual(event.data, {some: 'data', here: 'too'})
+  	assert.deepEqual(event.name, name)
+  	assert.deepEqual(event.data, data)
+  	assert.deepEqual(event.timestamp, timestamp)
   })
 })
